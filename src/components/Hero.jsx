f71afc646d355fff4569f6782b3a140d3f7cef9f
lokesh-bhortake <lb5102001@gmail.com>
2023-10-08 18:02:54 +0530

    Updated resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
   }
 
   return (
-    <section id='home' className={`flex text-accent py-40 md:py-32 lg:py-44 gap-2 ${styles.sizeFull}`}>
+    <section id='home' className={`flex text-accent py-36 md:py-32 lg:py-44 gap-2 ${styles.sizeFull}`}>
       <div className={`${styles.flexCenter} flex-col md:flex-row gap-2 w-full px-10 md:px-20 lg:px-0`}>
         
         <div className={`${styles.flexCenter} flex-col ${styles.sizeFull}  gap-6`}>
