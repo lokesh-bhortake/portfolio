601fb1d6f6b618ce1d3aaee32c0456eb84d89178
lokesh-bhortake <lb5102001@gmail.com>
2024-03-04 19:00:37 +0530

    Updated resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,42 +1,63 @@
-import React from 'react'
-import about_img from '../assets/images/about_img.svg';
-import { about_text, skills } from '../constants/constants';
-import styles from '../style';
+import React from "react";
+import about_img from "../assets/images/about_img.svg";
+import { about_text, skills } from "../constants/constants";
+import styles from "../style";
 
 const About = () => {
-  return (
-    <section id='about' className='text-accent gap-8 py-2'>
-      <div className='flex gap-8 items-center'>
-        <div className={`flex flex-col ${styles.sizeFull} gap-8 p-4`}>
-          <h3 className='text-h3 font-bold text-center'>About</h3>
-          <p className='text-base text-justify'>{about_text}</p>
-          <a href="#contact" className={`${styles.filledButton} md:w-[50%] text-center`}>Hire Me</a>
-        </div>
-        <div className={`hidden sm:flex ${styles.sizeFull}`}>
-          <img src={about_img} alt="about_img" className={`${styles.sizeFull}`}/>
-        </div>
-      </div>
+    return (
+        <section id="about" className="text-accent gap-8 py-2">
+            <div className="flex gap-8 items-center">
+                <div className={`flex flex-col ${styles.sizeFull} gap-8 p-4`}>
+                    <h3 className="text-h3 font-bold text-center">About</h3>
+                    <p className="text-base text-justify">{about_text}</p>
+                    <a
+                        href="#contact"
+                        className={`${styles.filledButton} md:w-[50%] text-center`}
+                    >
+                        Hire Me
+                    </a>
+                </div>
+                <div className={`hidden sm:flex ${styles.sizeFull}`}>
+                    <img
+                        src={about_img}
+                        alt="about_img"
+                        className={`${styles.sizeFull}`}
+                    />
+                </div>
+            </div>
 
-      <div className='relative items-center'>
-        <div className={`sm:hidden md:left-[25%] lg:left-[30%] layer-blur ${styles.blurDiv}`}></div>
-      </div>
+            <div className="relative items-center">
+                <div
+                    className={`sm:hidden md:left-[25%] lg:left-[30%] layer-blur ${styles.blurDiv}`}
+                ></div>
+            </div>
 
-      <div className={`${styles.flexCenter} flex-col gap-8 py-4`}>
-        <h3 className='text-h3 font-bold'>Skills</h3>
-        <ul className={`${styles.flexCenter} flex-wrap gap-x-6 gap-y-8 py-4 px-2`}>
-          {
-            skills.map((skill, index) => (
-              <li key={index} className={`${styles.flexCenter} flex-col gap-4 px-4 py-2`}>
-                <img src={skill.icon} alt={skill.text} className='w-12 h-12'/>
-                <p className='text-center text-base'>{skill.text}</p>
-              </li>
-            ))
-          }
-        </ul>
-      </div>
+            <div
+                className={`${styles.flexCenter} flex-col gap-8 py-20 sm:py-16 md:py-6`}
+            >
+                <h3 className="text-h3 font-bold">Skills</h3>
+                <ul
+                    className={`${styles.flexCenter} flex-wrap gap-x-6 gap-y-8 py-4 px-2`}
+                >
+                    {skills.map((skill, index) => (
+                        <li
+                            key={index}
+                            className={`${styles.flexCenter} flex-col gap-4 px-4 py-2`}
+                        >
+                            <img
+                                src={skill.icon}
+                                alt={skill.text}
+                                className="w-12 h-12"
+                            />
+                            <p className="text-center text-base">
+                                {skill.text}
+                            </p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    );
+};
 
-    </section>
-  )
-}
-
-export default About
+export default About;
--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,70 +1,85 @@
-import html from '../assets/icons/Html.svg'
-import css from '../assets/icons/CSS3.svg'
-import js from '../assets/icons/Js.svg'
-import python from '../assets/icons/Python.svg'
-import react from '../assets/icons/ReactJs.svg'
-import tailwind from '../assets/icons/Tailwind.svg'
-import mui from '../assets/icons/MUI.svg'
-import figma from '../assets/icons/Figma.svg'
-import rapidapi from '../assets/icons/RapidAPI.svg'
-import git from '../assets/icons/Git.svg'
-import linux from '../assets/icons/Linux.svg'
-import stripe from '../assets/icons/Stripe.svg'
-import mysql from '../assets/icons/MySQL.svg'
-import vscode from '../assets/icons/VS_code.svg'
-import fitflex_img from '../assets/images/fitflex.svg'
-import lbank_img from '../assets/images/lbank.svg'
-import portfolio_img from '../assets/images/portfolio.svg'
-import github from '../assets/icons/Github.svg'
-import mail from '../assets/icons/Mail.svg'
-import linkedin from '../assets/icons/Linkedin.svg'
-import resume from '../assets/files/Lokesh_Bhortake_Resume.pdf'
+import html from "../assets/icons/Html.svg";
+import css from "../assets/icons/CSS3.svg";
+import js from "../assets/icons/Js.svg";
+import python from "../assets/icons/Python.svg";
+import react from "../assets/icons/ReactJs.svg";
+import tailwind from "../assets/icons/Tailwind.svg";
+import mui from "../assets/icons/MUI.svg";
+import figma from "../assets/icons/Figma.svg";
+import rapidapi from "../assets/icons/RapidAPI.svg";
+import git from "../assets/icons/Git.svg";
+import linux from "../assets/icons/Linux.svg";
+import stripe from "../assets/icons/Stripe.svg";
+import mysql from "../assets/icons/MySQL.svg";
+import vscode from "../assets/icons/VS_code.svg";
+import symphony_img from "../assets/images/symphony.svg";
+import lbank_img from "../assets/images/lbank.svg";
+import portfolio_img from "../assets/images/portfolio.svg";
+import github from "../assets/icons/Github.svg";
+import mail from "../assets/icons/Mail.svg";
+import linkedin from "../assets/icons/Linkedin.svg";
+import resume from "../assets/files/Lokesh_Bhortake_Resume.pdf";
 
 export const navlinks = [
     {
-        text: 'Home',
-        link: 'hero',
+        text: "Home",
+        link: "hero",
     },
     {
-        text: 'About',
-        link: 'about',
-    }, 
-    {
-        text: 'Projects',
-        link: 'projects',
+        text: "About",
+        link: "about",
     },
     {
-        text: 'Contact',
-        link: 'contact',
-    }
-]
+        text: "Projects",
+        link: "projects",
+    },
+    {
+        text: "Contact",
+        link: "contact",
+    },
+];
 
-export const about_text = <><p style={{ textIndent: '2em' }}>I am a passionate and creative frontend developer ready to turn ideas into high quality websites with use of modern technologies.</p><br /><p style={{ textIndent: '2em' }}>I have Bachelor’s degree in Computer Science and Engineering. I am on a journey to create compelling web solutions that not only look amazing but also provide seamless interactions. I enjoy developing websites and also create designs to give them a good UI.</p><br /></>
+export const about_text = (
+    <>
+        <p style={{ textIndent: "2em" }}>
+            I am a passionate and creative frontend developer ready to turn
+            ideas into high quality websites with use of modern technologies.
+        </p>
+        <br />
+        <p style={{ textIndent: "2em" }}>
+            I have Bachelor’s degree in Computer Science and Engineering. I am
+            on a journey to create compelling web solutions that not only look
+            amazing but also provide seamless interactions. I enjoy developing
+            websites and also create designs to give them a good UI.
+        </p>
+        <br />
+    </>
+);
 
 export const skills = [
     {
         icon: html,
-        text: 'HTML5',
+        text: "HTML5",
     },
     {
         icon: css,
-        text: 'CSS3',
+        text: "CSS3",
     },
     {
         icon: js,
-        text: 'Javascript',
+        text: "Javascript",
     },
     {
         icon: python,
-        text: 'Python',
+        text: "Python",
     },
     {
         icon: react,
-        text: 'React',
+        text: "React",
     },
     {
         icon: tailwind,
-        text: 'Tailwind',
+        text: "Tailwind",
     },
     // {
     //     icon: mui,
@@ -72,7 +87,7 @@ export const skills = [
     // },
     {
         icon: figma,
-        text: 'Figma',
+        text: "Figma",
     },
     // {
     //     icon: rapidapi,
@@ -80,11 +95,11 @@ export const skills = [
     // },
     {
         icon: git,
-        text: 'Git',
+        text: "Git",
     },
     {
         icon: linux,
-        text: 'Linux',
+        text: "Linux",
     },
     // {
     //     icon: stripe,
@@ -96,83 +111,87 @@ export const skills = [
     // },
     {
         icon: vscode,
-        text: 'VS Code',
+        text: "VS Code",
     },
-]
+];
 
 export const projects = [
     {
-        image: fitflex_img,
-        title: 'FitFlex Gym Website',
-        description: 'Gym website which allows you to browse exercises, view demos of exercises and purchase gym plans with help of Stripe payment gateway.',
-        tech: 'React, MaterialUI, RapidAPI, Stripe.',
+        image: symphony_img,
+        title: "Symphony",
+        description:
+            "E commerce website which allows you to browse products, filter out products with responsive UI and efficient cart management",
+        tech: "React, Sanity, TailwindCSS",
         btn: [
             {
-                btn_text: 'Live',
-                btn_link: 'https://fitflexgym.netlify.app/',
+                btn_text: "Live",
+                btn_link: "https://symphonyecom.netlify.app/",
             },
             {
-                btn_text: 'Code',
-                btn_link: 'https://github.com/lokesh-bhortake/fitflex',
+                btn_text: "Code",
+                btn_link: "https://github.com/lokesh-bhortake/symphony",
             },
-        ]
+        ],
     },
     {
         image: portfolio_img,
-        title: 'Portfolio Site',
-        description: 'A website which showcases my coding journey and projects which is designed and is responsive for all the devices.',
-        tech: 'React, TailwindCSS, Figma',
+        title: "Portfolio Site",
+        description:
+            "A website which showcases my coding journey and projects which is designed and is responsive for all the devices.",
+        tech: "React, TailwindCSS, Figma",
         btn: [
             {
-                btn_text: 'Design',
-                btn_link: 'https://www.figma.com/file/PFMzQgEjgGCPV4wMUPuSHY/Portfolio-Site?type=design&node-id=0%3A1&mode=design&t=HHqpyOHVSHqy3eib-1',
+                btn_text: "Design",
+                btn_link:
+                    "https://www.figma.com/file/PFMzQgEjgGCPV4wMUPuSHY/Portfolio-Site?type=design&node-id=0%3A1&mode=design&t=HHqpyOHVSHqy3eib-1",
             },
             {
-                btn_text: 'Code',
-                btn_link: 'https://github.com/lokesh-bhortake/portfolio',
+                btn_text: "Code",
+                btn_link: "https://github.com/lokesh-bhortake/portfolio",
             },
-        ]
+        ],
     },
     {
         image: lbank_img,
-        title: 'Bank Landing Page',
-        description: 'A fully responsive bank landing page aimed to showcase the bank services and offerings with modern UI.',
-        tech: 'React, Tailwind CSS',
+        title: "Bank Landing Page",
+        description:
+            "A fully responsive bank landing page aimed to showcase the bank services and offerings with modern UI.",
+        tech: "React, Tailwind CSS",
         btn: [
             {
-                btn_text: 'Live',
-                btn_link: 'https://lbank.netlify.app/',
+                btn_text: "Live",
+                btn_link: "https://lbank.netlify.app/",
             },
             {
-                btn_text: 'Code',
-                btn_link: 'https://github.com/lokesh-bhortake/lbank',
+                btn_text: "Code",
+                btn_link: "https://github.com/lokesh-bhortake/lbank",
             },
-        ]
-    }
-]
+        ],
+    },
+];
 
 export const socials = [
     {
-        text: 'Github',
+        text: "Github",
         icon: github,
-        link: 'https://github.com/lokesh-bhortake',
+        link: "https://github.com/lokesh-bhortake",
     },
     {
-        text: 'Mail',
+        text: "Mail",
         icon: mail,
-        link: 'mailto:lb5102001@gmail.com',
+        link: "mailto:lb5102001@gmail.com",
     },
     {
-        text: 'LinkedIn',
+        text: "LinkedIn",
         icon: linkedin,
-        link: 'https://www.linkedin.com/in/lokesh-bhortake/',
+        link: "https://www.linkedin.com/in/lokesh-bhortake/",
     },
-]
+];
 
 export const openLink = (url) => {
-    window.open(url, '_blank');
-}
+    window.open(url, "_blank");
+};
 
 export const handleDownload = () => {
-    window.open(resume, '_blank');
+    window.open(resume, "_blank");
 };
